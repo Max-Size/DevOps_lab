fd5db048789b737b3f93b963fc077ed462677693
PC <maximuhkass14@gmail.com>
2023-06-01 17:34:08 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Person someone = new Person();
+        someone.get_info();
+        someone.print_info();
+    }
+}
